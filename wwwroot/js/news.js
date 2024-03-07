1008af4da20436b6027351eda16f535510a42054
Singharach Sa-ard <singharach.sa.63@ubu.ac.th>
2024-03-08 01:17:48 +0700

    update 65%

---
+++ wwwroot/js/news.js
@@ -0,0 +1,22 @@
+﻿let pagesIndex = 1;
+showPages(pagesIndex);
+
+function currentPages(n) {
+    showPages(pagesIndex = n);
+}
+
+function showPages(n) {
+    let i;
+    let pages = document.getElementsByClassName("news");
+    let buttons = document.getElementsByClassName("acm");
+    if (n > pages.length) { pagesIndex = 1 }
+    if (n < 1) { pagesIndex = pages.length }
+    for (i = 0; i < pages.length; i++) {
+        pages[i].style.display = "none";
+    }
+    for (i = 0; i < buttons.length; i++) {
+        buttons[i].className = buttons[i].className.replace(" activeB", "");
+    }
+    pages[pagesIndex - 1].style.display = "flex";
+    buttons[pagesIndex - 1].className += " activeB";
+}
