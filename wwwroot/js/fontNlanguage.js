14951ff6f3c83ff58846ab8281a6e29bacf57ddd
Singharach Sa-ard <singharach.sa.63@ubu.ac.th>
2024-03-11 00:03:45 +0700

    update nav

---
+++ wwwroot/js/fontNlanguage.js
@@ -0,0 +1,31 @@
+﻿let currentIndex = 1;
+let langIndex = 1;
+
+ChangeCurrent(currentIndex);
+Changelang(langIndex);
+
+function currentSelect(n) {
+    ChangeCurrent(currentIndex = n);
+}
+
+function currentLang(n) {
+    Changelang(langIndex = n);
+}
+
+function ChangeCurrent(n) {
+    let i;
+    let fonts = document.getElementsByClassName("fss");
+    for (i = 0; i < fonts.length; i++) {
+        fonts[i].className = fonts[i].className.replace(" activeC", "");
+    } 
+    fonts[currentIndex - 1].className += " activeC";
+}
+
+function Changelang(n) {
+    let i;
+    let langs = document.getElementsByClassName("lang-change");
+    for (i = 0; i < langs.length; i++) {
+        langs[i].className = langs[i].className.replace(" activeL", "");
+    }
+    langs[langIndex - 1].className += " activeL";
+}
